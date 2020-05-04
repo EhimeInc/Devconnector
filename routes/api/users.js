bb2bbb4c159e7f244a5a6464fff42fea353da89c
chuma615 <ctuwazurike@gmail.com>
2020-05-04 16:19:17 -0400

    creating and updating profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,7 @@ const gravatar = require('gravatar');
 const bcrpyt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {
-  check,
-  validationResult
-} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // User model
 const User = require('../../models/User');
@@ -25,38 +22,39 @@ router.post(
   [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a passwod with 8 or more characters').isLength({
-      min: 8
-    })
+    check(
+      'password',
+      'Please enter a passwod with 8 or more characters'
+    ).isLength({
+      min: 8,
+    }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
-        errors: errors.array()
+        errors: errors.array(),
       });
     }
 
     //  Destructure to pullout everthing in with req.body instead of doing it individually
 
-    const {
-      name,
-      email,
-      password
-    } = req.body;
+    const { name, email, password } = req.body;
 
     try {
       // See if user exists
 
       let user = await User.findOne({
-        email
+        email,
       });
 
       if (user) {
         return res.status(400).json({
-          errors: [{
-            msg: 'This user already exists'
-          }]
+          errors: [
+            {
+              msg: 'This user already exists',
+            },
+          ],
         });
       }
 
@@ -65,17 +63,17 @@ router.post(
       const avatar = gravatar.url(email, {
         s: '200', // default size or string length
         r: 'pg', // pg or parental guide "no adult content"
-        d: 'mm' // default multimedia image if user does not have a gravatar
-      })
+        d: 'mm', // default multimedia image if user does not have a gravatar
+      });
 
       user = new User({
         name,
         email,
         avatar,
-        password
+        password,
       });
       // Encrypt password
-      const salt = await bcrpyt.genSalt(16);
+      const salt = await bcrpyt.genSalt(10);
       user.password = await bcrpyt.hash(password, salt);
       await user.save();
 
@@ -83,25 +81,27 @@ router.post(
 
       const payload = {
         user: {
-          id: user.id
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000, // change to 3600 in production
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+          });
         }
-      }
-
-      jwt.sign(payload, config.get('jwtSecret'), {
-        expiresIn: 360000 // change to 3600 in production
-      }, (err, token) => {
-        if (err) throw err;
-        res.json({
-          token
-        });
-      });
-
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
-
     }
-
   }
 );
 
