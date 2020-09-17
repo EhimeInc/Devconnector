47e6c518739f77ad459d8fd57629b3acd5364dbe
chuma615 <ctuwazurike@gmail.com>
2020-09-16 22:12:36 -0400

    depend... update

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { rest } from 'lodash';
 
 const PrivateRoute = ({
   component: Component,
